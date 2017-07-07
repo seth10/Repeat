b6ee4dd7f9bd38cef1f028a18f6b46f45cde2379
Seth T <seth10@users.noreply.github.com>
2017-07-07 09:36:28 -0400

    Add delay option
    To avoid the function `unless` being called to quickly, a delay parameter is available. By default `unless` will be called (checked) twenty times a second. If you do not want any artificial delay, specify `delay=None` in your argument list.

--- Repeat.py
+++ Repeat.py
@@ -1,8 +1,10 @@
 import time
 
-def repeat(action, interval, unless):
+def repeat(action, interval, unless, delay=0.05):
     timeout = time.time()
     while not unless():
         if time.time() > timeout:
             timeout = time.time() + interval
             action()
+        if delay:
+            time.sleep(delay)
